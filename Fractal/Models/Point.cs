4adb35ed6477e82d30c98cf3f1a2677cd797fa69
dav bob <davdsb@gmail.com>
2020-06-23 22:31:44 +0300

    Create the Points class as Linked list with custom functions, such as "InsertRange"

---
+++ Fractal/Models/Point.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal.Models
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point Next { get; set; }
+        public Point Previous { get; set; }
+
+        public bool IsFirst => Previous == null;
+        public bool IsLast => Next == null;
+
+        public void AddAfter(Point p)
+        {
+            Next = p;
+            p.Previous = this;
+        }
+        public void AddBefore(Point p)
+        {
+            Previous = p;
+            p.Next = this;
+        }
+
+        public void InsertAfter(Point p)
+        {
+            Next?.AddBefore(p);
+            AddAfter(p);
+        }
+        public void InsertBefore(Point p)
+        {
+            Previous?.AddAfter(p);
+            AddBefore(p);
+        }
+
+        public void InsertRangeAfter(Point a, Point b)
+        {
+            Next?.AddBefore(b);
+            AddAfter(a);
+        }
+        public void InsertRangeBefore(Point a, Point b)
+        {
+            Previous?.AddAfter(a);
+            AddBefore(b);
+        }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Fractal/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private string _test;
+
+        public string Test
+        {
+            get { return _test; }
+            set { _test = value; OnPropertyChanged("Test"); }
+        }
+
+        public ViewModel()
+        {
+            Test = "Test";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+    }
+}
